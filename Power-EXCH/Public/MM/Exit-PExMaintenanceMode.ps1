9e1eb41903fdbf26e707c2e65552a41ccc0c9e1d
N8 <118857352+CoadMonkey@users.noreply.github.com>
2024-08-19 15:46:29 -0500

    Update Exit-PExMaintenanceMode.ps1

--- Power-EXCH/Public/MM/Exit-PExMaintenanceMode.ps1
+++ Power-EXCH/Public/MM/Exit-PExMaintenanceMode.ps1
@@ -17,7 +17,7 @@ Function Exit-PExMaintenanceMode
 	Version 1.0 :: 26-Dec-2021  :: [Release] :: Beta
 	Version 1.1 :: 03-Aug-2022  :: [Improve] :: Progress bar and steps counter, Parameter free function, Disk status check
     Version 1.2 :: 27-Jun-2024  :: [Improve] :: Add ability to run from admin workstation -CoadMonkey
-    Version 1.3 :: 08-Aug-2024  :: [Improve] :: Verbage updates, more verbose messages, additional checks. -CoadMonkey
+    Version 1.3 :: 19-Aug-2024  :: [Improve] :: Verbage updates, more verbose messages, additional checks. -CoadMonkey
                                 :: [Bugfix]  :: Errors if there are non-DAG databases -CoadMonkey
 .LINK
 	https://ps1code.com/2024/02/05/pexmm/
@@ -33,18 +33,20 @@ Function Exit-PExMaintenanceMode
 	
 	Begin
 	{
-	    $VerbosePreference = "Continue"
 		$FunctionName = '{0}' -f $MyInvocation.MyCommand
 		Write-Verbose "$FunctionName :: Started at [$(Get-Date)] on $Server" -Verbose:$True
+        Write-Verbose "Executing Get-ExchangeServer"
         If ($Server -notin (Get-ExchangeServer).name) {
             throw "$Server is not an Exchange server. Use -Server to specify a different name."
         }
         $RunLocal = $False
         If ($env:COMPUTERNAME -eq $Server) { $RunLocal = $True }
+        Write-Verbose "Executing Get-MailboxServer"
         $MailboxServer = Get-MailboxServer -Identity $Server
         If ( $MailboxServer.DatabaseAvailabilityGroup -eq $null ) {    # If Server is not a DAG member
             $TotalStep = 3
         } Else {
+            Write-Verbose "Executing Get-Cluster"
             If ($RunLocal) {
                 $DAG = (Get-Cluster).Name
             } Else {
@@ -65,6 +67,7 @@ Function Exit-PExMaintenanceMode
 						-Status "Exchange server: $($Server)" `
 						-CurrentOperation "Current operation: [Step $i of $TotalStep] Bring online $($DegradedDiskCount) degraded disks" `
 						-PercentComplete ($i/$($TotalStep) * 100)
+        Write-Verbose "Executing Get-Disk"
         If ($RunLocal) {
             $DegradedDisk = Get-Disk | Where-Object { $_.OperationalStatus -ne 'Online' -or $_.IsReadOnly }
         } Else {
@@ -82,11 +85,11 @@ Function Exit-PExMaintenanceMode
 				$DegradedDisk | Where-Object { $_.OperationalStatus -ne 'Online' } | Set-Disk -IsOffline:$false
 				$DegradedDisk | Where-Object { $_.IsReadOnly } | Set-Disk -IsReadOnly:$false
 			}
-            Write-Verbose "[Step $i of $TotalStep] $($DegradedDiskCount) degraded disks set to online and Read-Write"
+            Write-Verbose "[Step $i of $TotalStep] $($DegradedDiskCount) degraded disks set to online and Read-Write" -Verbose:$true
 		}
 		else
 		{
-			Write-Verbose "[Step $i of $TotalStep] All disks are Online"
+			Write-Verbose "[Step $i of $TotalStep] All disks are Online" -Verbose:$true
 		}
 		
 		### Activate Exchange componenets [ServerWideOffline] ###
@@ -97,27 +100,31 @@ Function Exit-PExMaintenanceMode
 						-PercentComplete ($i/$($TotalStep) * 100)
 		if ($PSCmdlet.ShouldProcess("Server [$($Server)]", "[Step $i of $TotalStep] Activate Exchange server components"))
 		{
-			Set-ServerComponentState $Server -Component ServerWideOffline -State Active -Requester Maintenance -Confirm:$false
+			Write-Verbose "Executing Set-ServerComponentState"
+            Set-ServerComponentState $Server -Component ServerWideOffline -State Active -Requester Maintenance -Confirm:$false
 			Set-ServerComponentState $Server -Component HubTransport -State Active -Requester Maintenance -Confirm:$false
     		$Count = 0
 			do
 			{
-				$Count += 10
+				$Count += 2
                 Write-Progress -Activity "Waiting for [Step $i]" `
 								-Status "Waiting for Exchange server componenets..." `
 								-PercentComplete ($Count) -Id 1
+                Write-Verbose "Executing Get-PExMaintenanceMode"
                 $ServerStatusAfter = Get-PExMaintenanceMode -Server $Server
-                Start-Sleep 1
 			}
 			while ($ServerStatusAfter.State -ne 'Connected' -and $Count -lt 100)
 		    Write-Progress -Activity "Completed" -Completed -Id 1
             If ($Count -ge 100)
             {
-                Write-Warning "[Step $i of $TotalStep] Timeout waiting for Exchange server componenets. Please wait and try again."
+                Write-Warning "[Step $i of $TotalStep] Timeout waiting for Exchange server componenets. Please wait and try again." -Verbose:$True
             }
-            Write-Verbose "[Step $i of $TotalStep] Exchange server componenets activated"
+            Else
+            {
+                Write-Verbose "[Step $i of $TotalStep] Exchange server componenets activated" -Verbose:$true
+            }
 		} Else {
-            Write-Verbose "[Step $i of $TotalStep] Exchange server componenets skipped"
+            Write-Verbose "[Step $i of $TotalStep] Exchange server componenets skipped" -Verbose:$true
         }
 		
 		### Resume DAG Cluster Node ###
@@ -127,7 +134,7 @@ Function Exit-PExMaintenanceMode
 							-Status "Exchange server: $($Server)" `
 							-CurrentOperation "Current operation: [Step $i of $TotalStep] Resume DAG Cluster Node" `
 							-PercentComplete ($i/$($TotalStep) * 100)
-            Write-Verbose "Checking cluster state..."
+            Write-Verbose "Executing Get-ClusterNode"
             If ($RunLocal) {
                 $ClusterNode = (Get-ClusterNode -Name $Server).State -eq 'Paused'
             } Else {
@@ -139,6 +146,7 @@ Function Exit-PExMaintenanceMode
 		    {
 			    if ($PSCmdlet.ShouldProcess("Server [$($Server)]", "[Step $i of $TotalStep] Resume DAG Cluster Node"))
 			    {
+                    Write-Verbose "Executing Resume-ClusterNode"
                     If ($RunLocal) {
                         Resume-ClusterNode -Name $Server | Out-Null
                     } Else {
@@ -146,12 +154,12 @@ Function Exit-PExMaintenanceMode
                             Resume-ClusterNode -Name $Using:Server | Out-Null
                         }
                     }
-                    Write-Verbose "[Step $i of $TotalStep] Resumed DAG Cluster Node"
+                    Write-Verbose "[Step $i of $TotalStep] Resumed DAG Cluster Node" -Verbose:$true
 			    } Else {
-                    Write-Verbose "[Step $i of $TotalStep] Resume DAG Cluster Node skipped"
+                    Write-Verbose "[Step $i of $TotalStep] Resume DAG Cluster Node skipped" -Verbose:$true
                 }
 		    } Else {
-                Write-Verbose "[Step $i of $TotalStep] DAG Cluster Node is already Up"
+                Write-Verbose "[Step $i of $TotalStep] DAG Cluster Node is already Up" -Verbose:$true
             }
 		}
 
@@ -164,7 +172,8 @@ Function Exit-PExMaintenanceMode
 						    -PercentComplete ($i/$($TotalStep) * 100)
 		    if ($PSCmdlet.ShouldProcess("Server [$($Server)]", "[Step $i of $TotalStep] Enable DB copy automatic activation"))
 		    {
-			    Set-MailboxServer $Server -DatabaseCopyAutoActivationPolicy Unrestricted -Confirm:$false
+			    Write-Verbose "Executing Set-MailboxServer"
+                Set-MailboxServer $Server -DatabaseCopyAutoActivationPolicy Unrestricted -Confirm:$false
 			    Set-MailboxServer $Server -DatabaseCopyActivationDisabledAndMoveNow:$false -Confirm:$false
     		    $Count = 0
                 $DatabaseCopyAutoActivationPolicy = (Get-MailboxServer $Server).DatabaseCopyAutoActivationPolicy
@@ -182,11 +191,11 @@ Function Exit-PExMaintenanceMode
 		        Write-Progress -Activity "Completed" -Completed -Id 1
                 If ($Count -ge 100)
                 {
-                    Write-Warning "[Step $i of $TotalStep] Timeout waiting for DatabaseCopyAutoActivationPolicy. Please wait and try again."
+                    Write-Warning "[Step $i of $TotalStep] Timeout waiting for DatabaseCopyAutoActivationPolicy. Please wait and try again." -Verbose:$true
                 }
-                Write-Verbose "[Step $i of $TotalStep] Enabled DB copy automatic activation"
+                Write-Verbose "[Step $i of $TotalStep] Enabled DB copy automatic activation" -Verbose:$true
 		    } Else {
-                Write-Verbose "[Step $i of $TotalStep] DB copy automatic activation Skipped"
+                Write-Verbose "[Step $i of $TotalStep] DB copy automatic activation Skipped" -Verbose:$true
             }
         }
 
@@ -200,25 +209,28 @@ Function Exit-PExMaintenanceMode
         {
             get-mailboxdatabase -Server $Server|? {$_.ReplicationType -eq "None"}|Set-MailboxDatabase -MountAtStartup:$True -Confirm:$False
             If ( $MailboxServer.DatabaseAvailabilityGroup -ne $null ) {    # Skip if Server is not a DAG member
+                Write-Verbose "Executing Move-ActiveMailboxDatabase"
                 Move-ActiveMailboxDatabase -ActivatePreferredOnServer $Server
+                Write-Verbose "Executing Mount-Database"
                 Get-MailboxDatabase -Server $Server|Mount-Database
-                Write-Verbose "[Step $i of $TotalStep] Rebalanced DAG / Mounted Database(s)"
+                Write-Verbose "[Step $i of $TotalStep] Rebalanced DAG / Mounted Database(s)" -Verbose:$true
 		    }
             Else
             {
+                Write-Verbose "Executing Mount-Database"
                 Get-MailboxDatabase -Server $Server|Mount-Database
-                Write-Verbose "[Step $i of $TotalStep] Mounted Databases"
+                Write-Verbose "[Step $i of $TotalStep] Mounted Databases" -Verbose:$true
             }
         }
         Else
         {
-            Write-Verbose "[Step $i of $TotalStep] Rebalance DAG / Mount Databases Skipped"
+            Write-Verbose "[Step $i of $TotalStep] Rebalance DAG / Mount Databases Skipped" -Verbose:$true
         }
 		Write-Progress -Activity "Completed" -Completed -Id 0
 	}
 	End
 	{
-        Return $ServerStatusAfter
 		Write-Verbose "$FunctionName :: Finished at [$(Get-Date)] on $Server" -Verbose:$True
+        Return $ServerStatusAfter
 	}	
 }
