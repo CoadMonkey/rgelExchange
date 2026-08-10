25d5cc9e85516fe58de4b1291d5cf785734d4c49
CoadMonkey <118857352+CoadMonkey@users.noreply.github.com>
2026-08-10 16:16:12 -0500

    Get-PExMaintenanceMode.ps1 Version 1.3
    Version 1.3 :: 10-Aug-2026  :: [Bugfix]  :: Sanitize user input to prevent false data from Get-ServerComponentState

--- Power-EXCH/Public/MM/Get-PExMaintenanceMode.ps1
+++ Power-EXCH/Public/MM/Get-PExMaintenanceMode.ps1
@@ -19,6 +19,7 @@ Function Get-PExMaintenanceMode
 	Version 1.0 :: 28-Nov-2021  :: [Release] :: Beta
 	Version 1.1 :: 28-Dec-2022  :: [Improve] :: New property TotalActiveComponent
 	Version 1.2 :: 21-Aug-2024  :: [Improve] :: Added offline check -CoadMonkey
+    Version 1.3 :: 10-Aug-2026  :: [Bugfix]  :: Sanitize user input to prevent false data from Get-ServerComponentState
 .LINK
 	https://ps1code.com/2024/02/05/pexmm/
 #>
@@ -34,9 +35,14 @@ Function Get-PExMaintenanceMode
 	Begin
 	{
 		#$WarningPreference = 'SilentlyContinue'
+        $ExchangeServers = (Get-ExchangeServer).name
 	}
 	Process
 	{
+
+        # Querying non-Exchange AD objects returns a deceptive, false-positive array of components.
+        If ($Server -notin $ExchangeServers) {Throw "$Server is not a valid Exchange Server."}
+
 		If (Test-Connection -Count 1 -Quiet $Server) {
             $State = if ($ComponentList = (Get-ServerComponentState $Server -ErrorAction Stop | Select-Object Component, State).Where{
 				    @('Monitoring', 'RecoveryActionsEnabled') -notcontains $_.Component -and $_.State -eq 'Active'
